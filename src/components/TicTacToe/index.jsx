edd0d39fce690666cfb31185b8dfa7e3b4a07d32
Rahul Jindal <rahul.jindaliit@gmail.com>
2024-02-23 00:28:27 +0530

    Optimise TicTacToe logic

--- src/components/TicTacToe/index.jsx
+++ src/components/TicTacToe/index.jsx
@@ -8,46 +8,49 @@ const defaultGameMatrix = [
 ];
 const defaultCurrentPlayer = "X";
 
+const checkRow = (row) => {
+  if (row.every((value) => value === row[0])) {
+    return row[0];
+  }
+  return null;
+};
+
 const getMatchWinner = (gameMatrix) => {
   const rows = gameMatrix.length;
   const columns = gameMatrix[0].length;
 
   // Row match
   for (let i = 0; i < rows; i++) {
-    let areValuesMismatched = false;
-    let currentValue;
-    for (let j = 0; j < columns - 1; j++) {
-      currentValue = gameMatrix[i][j];
-      if (!gameMatrix[i][j] || gameMatrix[i][j] !== gameMatrix[i][j + 1]) {
-        areValuesMismatched = true;
-      }
-    }
-    if (!areValuesMismatched) {
-      return currentValue;
+    const rowWinner = checkRow(gameMatrix[i]);
+    if (rowWinner) {
+      return rowWinner;
     }
   }
 
   // Column match
   for (let j = 0; j < columns; j++) {
-    let areValuesMismatched = false;
-    let currentValue;
-    for (let i = 0; i < rows - 1; i++) {
-      currentValue = gameMatrix[i][j];
-      if (!gameMatrix[i][j] || gameMatrix[i][j] !== gameMatrix[i + 1][j]) {
-        areValuesMismatched = true;
-      }
+    const columns = [];
+    for (let i = 0; i < rows; i++) {
+      columns.push(gameMatrix[i][j]);
     }
-    if (!areValuesMismatched) {
-      return currentValue;
+    const columnWinner = checkRow(columns);
+    if (columnWinner) {
+      return columnWinner;
     }
   }
 
-  // Diagonal Match
-  if (gameMatrix[0][0] === gameMatrix[1][1] && gameMatrix[2][2]) {
-    return gameMatrix[0][0];
+  // Check diagonals
+  const diagonal1 = [gameMatrix[0][0], gameMatrix[1][1], gameMatrix[2][2]];
+  const diagonal2 = [gameMatrix[0][2], gameMatrix[1][1], gameMatrix[2][0]];
+
+  const diagonal1Winner = checkRow(diagonal1);
+  if (diagonal1Winner) {
+    return diagonal1Winner;
   }
-  if (gameMatrix[0][2] === gameMatrix[1][1] && gameMatrix[2][0]) {
-    return gameMatrix[1][1];
+
+  const diagonal2Winner = checkRow(diagonal2);
+  if (diagonal2Winner) {
+    return diagonal2Winner;
   }
 
   return;
